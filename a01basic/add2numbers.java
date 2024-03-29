f82d2100f1415b27c94e7dab8a5b09421ff28ae4
shubham <shubham992284@gmail.com>
2024-03-29 10:31:34 +0530

    a01basic folder added

---
+++ a01basic/add2numbers.java
@@ -0,0 +1,11 @@
+package a01basic;
+import java.util.Scanner;
+public class add2numbers {
+ public static void main(String[] args) {
+	System.out.println("enter two numbers");
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	int b=sc.nextInt();
+	System.out.println("addtion of two no is"+(a+b));
+}
+}
